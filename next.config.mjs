f6d53ac7873af44572851a32cb277899a9a77c9d
hoonyboom <hyezoprk@kakao.com>
2024-07-19 03:05:55 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname),
+      '%': path.join(__dirname, 'components')
+    }
+  }
+};
 
 export default nextConfig;
