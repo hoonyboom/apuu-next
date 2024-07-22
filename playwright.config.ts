b7a12962590617ef39583b46d12ca97bc3f62a56
hoonyboom <hyezoprk@kakao.com>
2024-07-22 14:41:43 +0900

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,9 +65,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "pnpm run start",
-    url: "http://127.0.0.1:3001",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "pnpm run start",
+  //   url: "http://127.0.0.1:3001",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
