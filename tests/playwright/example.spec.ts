1854ddb4f1c4b7eb192737797c6698c615b0e9fa
hoonyboom <hyezoprk@kakao.com>
2024-07-24 18:11:06 +0900

    fix..

--- tests/playwright/example.spec.ts
+++ tests/playwright/example.spec.ts
@@ -1,12 +1,12 @@
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3001");
+  await page.goto("http://localhost:3000");
   await expect(page).toHaveTitle(/Apuu/);
 });
 
 test("has login link", async ({ page }) => {
-  await page.goto("http://localhost:3001");
+  await page.goto("http://localhost:3000");
   await page.getByRole("button", { name: "로그인" }).click();
   await expect(page.getByRole("heading", { name: "로그인" })).toBeVisible();
 });
