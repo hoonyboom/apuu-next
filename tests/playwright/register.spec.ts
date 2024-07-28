4f39b3914e6aa8ba0276be389256624e26fe6e7b
hoonyboom <hyezoprk@kakao.com>
2024-07-28 10:53:20 +0900

    port redo

--- tests/playwright/register.spec.ts
+++ tests/playwright/register.spec.ts
@@ -1,20 +1,20 @@
 import { expect, test } from "@playwright/test"
 
 test("타이틀이 있나요?", async ({ page }) => {
-  await page.goto("http://localhost:3001/register")
+  await page.goto("http://localhost:3000/register")
   await expect(page.getByText(/기본 정보/)).toBeInViewport()
 })
 
 test("다음 버튼이 있나요", async ({ page, isMobile }) => {
   test.skip(isMobile === false, "모바일에서만 적용되는 레이아웃입니다")
 
-  await page.goto("http://localhost:3001/register")
+  await page.goto("http://localhost:3000/register")
   const nextButton = page.getByRole("button", { name: "다음" })
   await expect(nextButton).toBeInViewport()
   await expect(nextButton).toBeDisabled()
 
   const select = page.getByRole("combobox")
-  await select.click()
+  await select.click();
 })
 // await page.locator(select).selectOption({ label: "수친자" });
 // await expect(nextButton).toBeEnabled();
--- tests/vitest/page.test.tsx
+++ tests/vitest/page.test.tsx
@@ -2,13 +2,13 @@ import { renderHook } from "@testing-library/react"
 import { useState } from "react"
 import { describe, expect, it } from "vitest"
 
-export const useHook = (length: number) => {
-  const [state] = useState(() => Array.from({ length }, v => false))
+export const useTransitionArray = (length: number) => {
+  const [state] = useState(() => Array.from({ length }, () => false))
   return state
 }
 
 describe("트랜지션 리스트 생성", () => {
-  const { result } = renderHook(() => useHook(15))
+  const { result } = renderHook(() => useTransitionArray(15))
   const state = result.current
 
   it("디폴트는 false", () => state.forEach(v => expect(v).toBeFalsy()))
