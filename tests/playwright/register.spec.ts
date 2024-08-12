0ca3620dc0c67dcb61a090b5c95da81db2b5c080
hoonyboom <hyezoprk@kakao.com>
2024-08-13 08:11:56 +0900

    test

--- tests/playwright/register.spec.ts
+++ tests/playwright/register.spec.ts
@@ -2,19 +2,23 @@ import { expect, test } from "@playwright/test"
 
 test("타이틀이 있나요?", async ({ page }) => {
   await page.goto("http://localhost:3000/register")
-  await expect(page.getByText(/기본 정보/)).toBeInViewport()
+  await expect(page.getByText(/Apuu/)).toBeInViewport()
 })
 
-test("다음 버튼이 있나요", async ({ page, isMobile }) => {
-  test.skip(isMobile === false, "모바일에서만 적용되는 레이아웃입니다")
-
+test("카카오 로그인 버튼이 있나요?", async ({ page }) => {
   await page.goto("http://localhost:3000/register")
-  const nextButton = page.getByRole("button", { name: "다음" })
-  await expect(nextButton).toBeInViewport()
-  await expect(nextButton).toBeDisabled()
-
-  const select = page.getByRole("combobox")
-  await select.click()
+  const kakaoButton = page.getByRole("button", { name: "카카오 로그인" })
+  await expect(kakaoButton).toBeInViewport()
 })
-// await page.locator(select).selectOption({ label: "수친자" });
-// await expect(nextButton).toBeEnabled();
+
+// test("다음 버튼이 있나요", async ({ page, isMobile }) => {
+//   test.skip(isMobile === false, "모바일에서만 적용되는 레이아웃입니다")
+
+//   await page.goto("http://localhost:3000/register")
+//   const nextButton = page.getByRole("button", { name: "다음" })
+//   await expect(nextButton).toBeInViewport()
+//   await expect(nextButton).toBeDisabled()
+
+//   const select = page.getByRole("combobox")
+//   await select.click()
+// })
